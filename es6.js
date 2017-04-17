6e98d64c3303a1f1c423d0307a7100772e435938
Lian Rodriguez <lian_rr@hotmail.com>
2017-04-16 20:18:08 -0600

    all elements draw and the paddle have the events handlers. Missing collitions

--- es6.js
+++ es6.js
@@ -58,28 +58,12 @@ class Paddle extends Figure {
         ctx.closePath();
     }
 
-    keyDownHandler(e) {
-        if (e.keyCode == 39) {
-            this.rightPressed = true;
+    move() {
+        if (this.rightPressed && this.x < this.canvas.width - this.width) {
+            this.x += 7;
         }
-        else if (e.keyCode == 37) {
-            this.leftPressed = true;
-        }
-    }
-
-    keyUpHandler(e) {
-        if (e.keyCode == 39) {
-            this.rightPressed = false;
-        }
-        else if (e.keyCode == 37) {
-            this.leftPressed = false;
-        }
-    }
-
-    mouseMoveHandler(e) {
-        let relativeX = e.clientX - this.canvas.offsetLeft;
-        if (relativeX > 0 && relativeX < this.canvas.width) {
-            this.x = relativeX - this.width / 2;
+        else if (this.leftPressed && this.x > 0) {
+            this.x -= 7;
         }
     }
 
@@ -107,17 +91,77 @@ class Brick extends Figure {
 
 //---------------------------------------Game--------------------------------------------------
 class Game {
-    constructor(canvas, ball, paddle) {
+    constructor(canvas, ball, paddle, brickColumnCount, brickRowCount) {
         this.canvas = canvas;
         this.ball = ball;
         this.paddle = paddle;
+        this.brickColumnCount = brickColumnCount;
+        this.brickRowCount = brickRowCount;
+        this.bricks = [];
+
+        this.createBricks(75,20);
+    }
+
+    mouseMoveHandler(e) {
+        let relativeX = e.clientX - this.canvas.offsetLeft;
+        if (relativeX > 0 && relativeX < this.canvas.width) {
+            paddle.x = relativeX - paddle.width / 2;
+        }
+    }
+
+    keyDownHandler(e) {
+        if (e.keyCode == 39) {
+            this.paddle.rightPressed = true;
+        }
+        else if (e.keyCode == 37) {
+            this.paddle.leftPressed = true;
+        }
+    }
+
+    keyUpHandler(e) {
+        if (e.keyCode == 39) {
+            this.paddle.rightPressed = false;
+        }
+        else if (e.keyCode == 37) {
+            this.paddle.leftPressed = false;
+        }
+    }
+
+
+    createBricks(brickWidth, brickHeight) {
+        for (let i = 0; i < this.brickColumnCount; i++) {
+            this.bricks[i] = [];
+            for (let j = 0; j < this.brickRowCount; j++) {
+                // bricks[i][j] = {x: 0, y: 0, status: 1};
+                this.bricks[i][j] = new Brick(0,0,brickWidth, brickHeight);
+            }
+        }
+    }
+
+    drawBricks(ctx, brickPadding, brickOffsetLeft, brickOffsetTop) {
+        for (let i = 0; i < this.brickColumnCount; i++) {
+            for (let j = 0; j < this.brickRowCount; j++) {
+                let brick = this.bricks[i][j];
+                if (brick.lives >= 1) {
+                    let brickX = (i * (brick.width + brickPadding)) + brickOffsetLeft;
+                    let brickY = (j * (brick.height + brickPadding)) + brickOffsetTop;
+
+                    brick.x = brickX;
+                    brick.y = brickY;
+                    brick.draw(ctx);
+
+                }
+            }
+        }
     }
 
     play(ctx) {
         this.ball.draw(ctx);
         this.paddle.draw(ctx);
+        this.drawBricks(ctx,10,30,30);
 
         this.ball.move();
+
 
         if (this.ball.x + this.ball.dx > this.canvas.width - this.ball.radius || this.ball.x + this.ball.dx < this.ball.radius) {
             ball.dx = -ball.dx;
@@ -125,6 +169,9 @@ class Game {
         if (this.ball.y + this.ball.dy > this.canvas.height - this.ball.radius || this.ball.y + this.ball.dy < this.ball.radius) {
             this.ball.dy = -this.ball.dy;
         }
+
+
+        this.paddle.move();
 
     }
 }
@@ -135,8 +182,13 @@ const ctx = canvas.getContext("2d");
 ball = new Ball(canvas, 10);
 paddle = new Paddle(canvas, 10, 75);
 
-game = new Game(canvas,ball,paddle);
+game = new Game(canvas, ball, paddle, 5, 3);
 
+
+document.addEventListener("keydown", e => game.keyDownHandler(e), false);
+document.addEventListener("keyup", e => game.keyUpHandler(e), false);
+
+// document.addEventListener("mousemove", e => game.mouseMoveHandler(), false);
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
